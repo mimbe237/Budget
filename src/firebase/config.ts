051b79f74ab549fbfadd179e8ddc1417b625b7c7
THEOPHILE MIMBE NOUTCHOM <businessclubleader7@gmail.com>
2025-10-09 12:38:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3821270625-cd276",
+  "appId": "1:961816213503:web:13f880d3a00d50a31419ca",
+  "apiKey": "AIzaSyAWXdkN1Lr2oFhg-4CR_4VN1vmNp5DAn9E",
+  "authDomain": "studio-3821270625-cd276.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "961816213503"
+};
